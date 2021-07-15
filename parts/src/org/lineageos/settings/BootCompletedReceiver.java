bbd9e8abae533be5eda649955aa2b57c5d7b2d8e
anuragbhaumik <bhaumik463@gmail.com>
2021-07-15 21:33:50 +0530

    Revert: DC-dimming and HBM
    Revert "raphael: parts:  Add HBM qs tile"
    
    This reverts commit 0a44e9b4b7dc7d1fc2acb2556a1fcf7be9462d3a.
    
    Revert "raphael: parts: Set priority for Display Settings switch"
    
    This reverts commit 8f5fa8340421cc6def323b136cb5e5558d007ba6.
    
    Revert "rapahel: parts: Implement HBM switch"
    
    This reverts commit e9c03a8b216cf0cdd9eb73e61faf4de3938f9931.
    
    Revert "raphael: parts: Add DC-Dimming QS tile"
    
    This reverts commit d2694a1134667ca91d19221be21714a1d53c0dfb.
    
    Revert "raphael: parts: implement dc dimming preference"
    
    This reverts commit a2ffe42fd17e5ef8c6521083b5617c5df86a4baf.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -25,18 +25,10 @@ import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
-import org.lineageos.settings.utils.FileUtils;
-import android.content.SharedPreferences;
-import androidx.preference.PreferenceManager;
-
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
-    private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
-    private static final String DC_DIMMING_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/msm_fb_ea_enable";
-    private static final String HBM_ENABLE_KEY = "hbm_mode";
-    private static final String HBM_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/hbm";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
@@ -51,11 +43,5 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         PopupCameraUtils.startService(context);
         ThermalUtils.initialize(context);
 
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-
-        boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
-        FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
-        boolean hbmEnabled = sharedPrefs.getBoolean(HBM_ENABLE_KEY, false);
-        FileUtils.writeLine(HBM_NODE, hbmEnabled ? "1" : "0");
     }
 }
